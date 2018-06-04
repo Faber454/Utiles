dffa965fb140e99bab5ef7e8871ed49b3df0c621
unknown <Elkin>
2018-06-04 13:56:44 -0500

    Recycler View plantilla

--- utiles_lib/src/main/java/com/efom/utiles_lib/Adapters/RecyclerViewHolder.java
+++ utiles_lib/src/main/java/com/efom/utiles_lib/Adapters/RecyclerViewHolder.java
@@ -5,6 +5,7 @@ import android.view.View;
 /*
 import android.view.View;
 import android.widget.TextView;
+
 */
 public class RecyclerViewHolder /*extends RecyclerView.ViewHolder */ {
     //public TextView tV_titulo, tV_descripcion, tV_hora;
