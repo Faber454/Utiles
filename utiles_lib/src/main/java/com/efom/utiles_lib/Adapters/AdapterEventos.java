36b068daee25aacffe3770629d5c780ca4493e3f
unknown <Elkin>
2018-06-04 13:55:06 -0500

    Recycler View plantilla

--- utiles_lib/src/main/java/com/efom/utiles_lib/Adapters/AdapterEventos.java
+++ utiles_lib/src/main/java/com/efom/utiles_lib/Adapters/AdapterEventos.java
@@ -1,19 +1,14 @@
 package com.efom.utiles_lib.Adapters;
-/*
-import android.support.v7.widget.CardView;
-import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 import java.util.ArrayList;
 
-public class AdapterEventos extends RecyclerView.Adapter<RecyclerViewHolder> {
+public class AdapterEventos /*extends RecyclerView.Adapter<RecyclerViewHolder> */{
     ArrayList<String[]> eventos;
 
     public AdapterEventos (ArrayList<String[]> eventos){
         this.eventos = eventos;
     }
+/*
 
     @Override
     public RecyclerViewHolder onCreateViewHolder(ViewGroup AdapterEventos, int viewType) {
@@ -33,5 +28,5 @@ public class AdapterEventos extends RecyclerView.Adapter<RecyclerViewHolder> {
     public int getItemCount() {
         return eventos.size();
     }
-
-}*/
+*/
+}
--- utiles_lib/src/main/java/com/efom/utiles_lib/Adapters/RecyclerViewHolder.java
+++ utiles_lib/src/main/java/com/efom/utiles_lib/Adapters/RecyclerViewHolder.java
@@ -1,14 +1,17 @@
 package com.efom.utiles_lib.Adapters;
+
+import android.view.View;
+
 /*
 import android.view.View;
 import android.widget.TextView;
-
-public class RecyclerViewHolder extends RecyclerView.ViewHolder {
-    public TextView tV_titulo, tV_descripcion, tV_hora;
+*/
+public class RecyclerViewHolder /*extends RecyclerView.ViewHolder */ {
+    //public TextView tV_titulo, tV_descripcion, tV_hora;
     public RecyclerViewHolder(View itemView) {
-        super(itemView);
-        tV_titulo      = (TextView)itemView.findViewById(R.id.tV_titulo);
-        tV_descripcion = (TextView)itemView.findViewById(R.id.tV_descripcion);
-        tV_hora        = (TextView)itemView.findViewById(R.id.tV_hora);
+        //super(itemView);
+        //tV_titulo      = (TextView)itemView.findViewById(R.id.tV_titulo);
+        //tV_descripcion = (TextView)itemView.findViewById(R.id.tV_descripcion);
+        //tV_hora        = (TextView)itemView.findViewById(R.id.tV_hora);
     }
-}*/
+}
